74d284316a13150ada0a3038a21a4c51b9b18eff
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2023-11-04 02:05:54 +0700

    day2

--- src/ShowProduct/ShowProduct.jsx
+++ src/ShowProduct/ShowProduct.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import { Item } from './Item';
 import {AiFillStar} from "react-icons/ai";
 import "./Item.css";
+import  Carousel  from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ShowProduct = () => {
-  return (
+  return (<>
     <div className='products'>
         {Item.map((product) => (
                 <div key={product.id} className='product' >
@@ -22,6 +24,34 @@ const ShowProduct = () => {
         ))
         }
     </div>
+
+    <p className='goiY'><b>Gợi ý</b></p>
+
+    <Carousel data-bs-theme="dark" className='itemSlow'>
+     {Item.map((product) => (
+        // eslint-disable-next-line react/jsx-key
+        <Carousel.Item className='itemSlow'>
+        <img
+          className="anh2"
+          src={product.image}
+          alt={product.name}
+        />
+         <Carousel.Caption>
+          <p className='name'>{product.name}</p>
+          <p className='name'><b>{product.name}</b></p>
+          <div className='star'>
+                {Array.from(Array(Math.ceil(product.rate)).keys()).map((star) => (
+                    <AiFillStar key={star} color='yellow'/>
+                ))}
+            </div>
+            <div className='price'><b>{product.price} VND</b></div>
+            <button className='them2' >Thêm vào giỏ hàng</button>          
+        </Carousel.Caption>
+      </Carousel.Item>
+    ))} 
+    </Carousel>
+      
+    </>
   )
 }
 
